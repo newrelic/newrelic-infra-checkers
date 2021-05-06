6d5c5285bc9b293fc6b89d6042077c9da89bc9a9
Alvaro Cabanas <acabanas@newrelic.com>
2021-05-06 17:51:20 +0200

    fix: modify if in script

--- test/test_local.sh
+++ test/test_local.sh
@@ -5,7 +5,7 @@ set -o errexit
 for file in test/data/*
 do
     RESULT=$(diff  "./."$(basename $file) $file) || true
-    if [[ $RESULT = "" ]]; then
+    if [ "$RESULT" == "" ]; then
         echo "✅ The generated file $(basename $file) is as expected"
     else
         echo "❌ The generated file $(basename $file) is not as expected"
