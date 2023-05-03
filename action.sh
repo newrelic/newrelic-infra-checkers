fdcf00206293fc35b4b0ef2b04d405c80bb8c669
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2023-05-03 16:36:59 +0200

    add release toolkit dictionary

--- action.sh
+++ action.sh
@@ -6,6 +6,7 @@ set -o pipefail
 [[ -n $GITHUB_ACTION_PATH ]] || GITHUB_ACTION_PATH=$(pwd)
 [[ -n $SEMGREP_APPEND ]] || SEMGREP_APPEND="true"
 [[ -n $GOLANGCI_LINT_CONFIG ]] || GOLANGCI_LINT_CONFIG="golangci-lint-limited"
+[[ -n $RT_DICTIONARY ]] || RT_DICTIONARY="rt-dictionary"
 
 echo "Using $GOLANGCI_LINT_CONFIG golangci-lint configuration folder"
 
@@ -41,6 +42,22 @@ do
     else
       echo "ℹ️ Local $fileBasename file detected skipping overwrite"
     fi
+done
+
+
+echo "Using $RT_DICTIONARY release toolkit dictionary folder"
+
+find $GITHUB_ACTION_PATH/$RT_DICTIONARY  -type f | while read -r file
+do
+  fileBasename=$(basename $file)
+
+  if [[ ! -f "$fileBasename" ]]
+  then
+      echo "ℹ️ Copying $fileBasename file to repo root directory"
+      cp "$file" "$fileBasename"
+  else
+      echo "ℹ️ Local $fileBasename file detected skipping overwrite"
+  fi
 done
 
 echo "ℹ️ Configuration files correctly copied"
