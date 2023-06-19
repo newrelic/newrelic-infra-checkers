82d8a0ae760cd5bb83a1344be119bad82fba5394
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-06-19 17:45:26 +0200

    add access from github role to new canaries bucket

--- terraform_modules/base_framework/iam-team-user.tf
+++ terraform_modules/base_framework/iam-team-user.tf
@@ -40,6 +40,39 @@ resource aws_iam_policy access_to_terraform_states {
   })
 }
 
+resource aws_iam_policy access_to_new_terraform_states {
+  name        = "access-to-new-terraform-states"
+  description = "This policy allows to store and access tfstates in the bucket coreint-canaries"
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::coreint-canaries"
+        Action   = [
+          "s3:ListBucket"
+        ]
+      }, {
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::coreint-canaries/*"
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+      }, {
+        Effect   = "Allow",
+        Resource = "arn:aws:dynamodb:*:*:table/coreint-canaries"
+        Action   = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+      }
+    ]
+  })
+}
+
 resource aws_iam_policy access_to_eks_cluster {
   name        = "access-to-eks-cluster"
   description = "This policy allows to access all EKS clusters in this account"
@@ -91,6 +124,11 @@ resource aws_iam_policy access_to_eks_cluster {
 resource aws_iam_user_policy_attachment coreint_access_to_terraform_states {
   user       = aws_iam_user.coreint.name
   policy_arn = aws_iam_policy.access_to_terraform_states.arn
+}
+
+resource aws_iam_user_policy_attachment coreint_access_to_new_terraform_states {
+  user       = aws_iam_user.coreint.name
+  policy_arn = aws_iam_policy.access_to_new_terraform_states.arn
 }
 
 resource aws_iam_user_policy_attachment coreint_access_to_eks_cluster {
